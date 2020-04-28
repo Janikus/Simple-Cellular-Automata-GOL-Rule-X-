5b6ac5b520b7092f725116a40d82eb7174d05924
Janikus <64164163+Janikus@users.noreply.github.com>
2020-04-28 19:01:58 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,7 +1,8 @@
 import main
 
 
-option = int(input("Select an option \n1. Conway's Game of Life\n2. Rule x CA\nOption: "))
+option = int(input("Select an option \n1. Conway's Game of Life\n2. Rule x CA\n3. Sum x algorithm\n4. Code x "
+                   "algorithm\nOption: "))
 
 if option == 1:
     dimensions = int(input("Select a number of cells (for both width and height) (recommended: 100): "))
@@ -12,5 +13,20 @@ elif option == 2:
     ruleX = main.RuleX(iterations * 2 + 3, iterations)
     rule = int(input("Select a rule: "))
     ruleX.set_rule(rule)
+    ruleX.change_time_step(0)
     ruleX.game()
-
+elif option == 3:
+    iterations = int(input("Select a number of iterations (recommended: 100): "))
+    multX = main.MultX(iterations * 2 + 3, iterations)
+    step = int(input("Select a rule: "))
+    multX.set_step(step)
+    multX.change_time_step(0)
+    multX.game()
+elif option == 4:
+    dimensions = int(input("Select a number of cells (for both width and height) (recommended: 100): "))
+    codeX = main.CodeX(dimensions, dimensions)
+    rule = int(input("Select a rule: "))
+    codeX.set_rule(rule)
+    codeX.game()
+else:
+    print("Option not available")
